0aeb409b6433b18ac4bb3be0382044a8d107c742
AliefEI <77537630+AliefEI@users.noreply.github.com>
2021-03-28 11:54:43 -0400

    update on bugs
    completed: 5
    feel good: 5

--- public/main.js
+++ public/main.js
@@ -3,93 +3,105 @@ var trash = document.getElementsByClassName("fa-trash");
 var thumbDown = document.getElementsByClassName("fa-thumbs-down");
 var changeColor = document.getElementsByClassName("change")
 
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const quote = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('thumbUp', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'quote': quote,
-            'thumbUp':thumbUp
-          })
+Array.from(thumbUp).forEach(function (element) {
+  element.addEventListener('click', function () {
+    const listItem = this.closest('.message')
+    const name = listItem.querySelector('.nameClass').innerText
+    const quote = listItem.querySelector('.quoteClass').innerText
+    const thumbUp = parseFloat(listItem.querySelector('.thumbUPS').innerText)
+    fetch('thumbUp', {
+        method: 'put',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'name': name,
+          'quote': quote,
+          'thumbUp': thumbUp
         })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
+      })
+      // .then(response => {
+      //   if (response.ok) return response.json()
+      // })
+      .then(data => {
+        console.log(data)
+        window.location.reload(true)
+      })
+  });
 });
 
-Array.from(thumbDown).forEach(function(element) {
-  element.addEventListener('click', function(){
+Array.from(thumbDown).forEach(function (element) {
+  element.addEventListener('click', function () {
+    const listItem = this.closest('.message')
+    const name = listItem.querySelector('.nameClass').innerText
+    const quote = listItem.querySelector('.quoteClass').innerText
+    const thumbDown = parseFloat(listItem.querySelector('.thumbDOWNS').innerText)
+    fetch('thumbDown', {
+        method: 'put',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'name': name,
+          'quote': quote,
+          "thumbDown": thumbDown
+        })
+      })
+      // .then(response => {
+      //   if (response.ok) return response.json()
+      // })
+      .then(data => {
+        console.log(data)
+        window.location.reload(true)
+      })
+  });
+});
+
+Array.from(changeColor).forEach(function (element) {
+  element.addEventListener('click', function () {
+    //'closeest'--target closest css selector
+    const listItem = this.closest('.message')
+    //innerHTML grabs everything, innerTEXT grabs the text 
+    const name = listItem.querySelector('.nameClass').innerText
+    const quote = listItem.querySelector('.quoteClass').innerText
+    //parseFloat--change to a number
+    const thumbUp = parseFloat(listItem.querySelector('.thumbUPS').innerText)
+    fetch('coloredChange', {
+        method: 'put',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'name': name,
+          'quote': quote,
+          'thumbUp': thumbUp
+        })
+      })
+      .then(response => {
+        if (response.ok) return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        window.location.reload(true)
+      })
+  });
+});
+
+Array.from(trash).forEach(function (element) {
+  element.addEventListener('click', function () {
     const name = this.parentNode.parentNode.childNodes[1].innerText
     const quote = this.parentNode.parentNode.childNodes[3].innerText
-    const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[7].innerText)
-    fetch('thumbDown', {
-      method: 'put',
-      headers: {'Content-Type': 'application/json'},
+    fetch('messages', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify({
         'name': name,
-        'quote': quote,
-        "thumbDown":thumbDown
+        'quote': quote
       })
-    })
-    .then(response => {
-      if (response.ok) return response.json()
-    })
-    .then(data => {
-      console.log(data)
-      window.location.reload(true)
+    }).then(function (response) {
+      window.location.reload()
     })
   });
-});
-
-Array.from(changeColor).forEach(function(element) {
-  element.addEventListener('click', function(){
-    const name = this.parentNode.parentNode.childNodes[1].innerText
-    const quote = this.parentNode.parentNode.childNodes[3].innerHTML
-    const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-    fetch('coloredChange', {
-      method: 'put',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        'name': name,
-        'quote': quote,
-        'thumbUp':thumbUp
-      })
-    })
-    .then(response => {
-      if (response.ok) return response.json()
-    })
-    .then(data => {
-      console.log(data)
-      window.location.reload(true)
-    })
-  });
-});
-
-Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const quote = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'quote': quote
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
 });
--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.post('/messages', (req, res) => {
   db.collection('messages').save({
     name: req.body.name, 
     quote: req.body.quote, 
-    color: "black"
-    // thumbUp: 0, 
-    // thumbDown:0
+    color: "black",
+    thumbUp: 0, 
+    thumbDown:0
   }, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
@@ -45,35 +45,39 @@ app.post('/messages', (req, res) => {
   })
 })
 
-// app.put('/thumbUp', (req, res) => {
-//   db.collection('messages')
-//   .findOneAndUpdate({name: req.body.name, quote: req.body.quote}, {
-//     $set: {
-//       thumbUp:req.body.thumbUp + 1
-//     }
-//   }, {
-//     sort: {_id: -1},
-//     upsert: true
-//   }, (err, result) => {
-//     if (err) return res.send(err)
-//     res.send(result)
-//   })
-// })
+app.put('/thumbUp', (req, res) => {
+  db.collection('messages')
+  .findOneAndUpdate({
+    name: req.body.name, 
+    quote: req.body.quote}, {
+    $set: {
+      thumbUp:req.body.thumbUp + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
 
-// app.put('/thumbDown', (req, res) => {
-//   db.collection('messages')
-//   .findOneAndUpdate({name: req.body.name, quote: req.body.quote}, {
-//     $set: {
-//       thumbDown:req.body.thumbDown + 1
-//     }
-//   }, {
-//     sort: {_id: -1},
-//     upsert: true
-//   }, (err, result) => {
-//     if (err) return res.send(err)
-//     res.send(result)
-//   })
-// })
+app.put('/thumbDown', (req, res) => {
+  db.collection('messages')
+  .findOneAndUpdate({
+    name: req.body.name, 
+    quote: req.body.quote}, {
+    $set: {
+      thumbDown:req.body.thumbDown + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
 
 app.put('/coloredChange', (req, res) => {
   db.collection('messages')
